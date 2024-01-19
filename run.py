073fee0333486a88ff40f294373b662c252a9498
Notebooked <creepinglion09@gmail.com>
2024-01-19 08:38:20 -0800

    run script refactor

--- run.py
+++ run.py
@@ -1,25 +1,21 @@
-import argparse
-
 import torch
 import torchvision.models
 import torchvision.transforms as transforms
-from PIL import Image
 
 from reshape import reshape_model
 
-parser = argparse.ArgumentParser(description='Run Image Classifier')
+def load_model():
+    model = torchvision.models.__dict__[SAVED_PTH['arch']]()
+    model = reshape_model(model, SAVED_PTH['num_classes'])
+    model.load_state_dict(SAVED_PTH['state_dict'])
+    model.eval()
+    return model
 
-parser.add_argument('--img', type=str, default='data/test/TomatoYellowCurlVirus1.JPG', 
-                    help='path to desired image to run model on'
-					'image path (default: data/test/PotatoHealthy1.JPG/)')
+MODEL_PATH = "original_model_best.pth.tar"
+SAVED_PTH = torch.load(MODEL_PATH)
+MODEL = load_model()
 
-args = parser.parse_args()
-
-#TODO: put the god forsaken global model variable so you dont need to reload the model everytime
-
-def create_image_tensor(img_path, resolution):
-    img = Image.open(img_path)
-
+def create_image_tensor(pil_img, resolution):
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
         
@@ -30,30 +26,19 @@ def create_image_tensor(img_path, resolution):
         normalize,
     ])
 
-    t = val_transforms(img)
+    t = val_transforms(pil_img)
 
     t = t.unsqueeze(0)
 
     return t
 
-def inference_image(img):
-    saved_pth = torch.load("models34/model_best.pth.tar")
+def inference_image(pil_img):
+    img_tensor = create_image_tensor(pil_img, SAVED_PTH['resolution'])
 
-    model = torchvision.models.__dict__[saved_pth['arch']]()
-
-    model = reshape_model(model, saved_pth['num_classes'])
-    model.load_state_dict(saved_pth['state_dict'])
-    model.eval()
-
-    img_tensor = create_image_tensor(img, saved_pth['resolution'])
-
-    output = model(img_tensor)
+    output = MODEL(img_tensor)
     prediction_idx = torch.argmax(output).numpy()
     confidence = torch.max(torch.nn.Softmax(dim=1)(output)).detach().numpy() * 100
 
-    prediction = saved_pth['classes'][prediction_idx]
+    prediction = SAVED_PTH['classes'][prediction_idx]
 
     return (prediction, confidence)
-
-if __name__ == "__main__":
-    print(inference_image(args.img))
